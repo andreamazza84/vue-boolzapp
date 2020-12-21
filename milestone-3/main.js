9d750680fca39360e39a292b0cc71dc70142cd49
andreamazza84 <andrea.mazza84@gmail.com>
2020-12-21 12:40:40 +0100

    Milestone 3 ok

--- milestone-3/main.js
+++ milestone-3/main.js
@@ -91,31 +91,15 @@ let app = new Vue({
     counter: 0,
     userName: "Michele",
     userAvatar: "_1",
-    userMsg: [{
-        date: '10/01/2020 15:30:55',
-        text: 'Hai portato a spasso il cane?',
-        status: 'sent'
-        },
-        {
-        date: '10/01/2020 15:50:00',
-        text: 'Ricordati di dargli da mangiare',
-        status: 'sent'
-        },
-        {
-        date: '10/01/2020 16:15:22',
-        text: 'Tutto fatto!',
-        status: 'received'
-        }],
-    
-    userObj: {},
-    
+       
     // MS3 
-    typingMsg: "",
-    inputMsg: "",
-    sentMessages: [],
-    responseMsg: "ok",
-
-    }, //data
+    typing: "", //cache messaggi
+    message: "",
+    //MS3 - data
+    date: dayjs().format('DD/MM/YYYY HH:mm:ss'),  
+    }, 
+    
+    
 
     methods: {
     //MS2
@@ -123,34 +107,31 @@ let app = new Vue({
         chatFilter: function(name, list){
             list.forEach((element, index) => {
                 if (element.name === name) {
-                    //ricavo l'oggetto e l'indice dall'array filtrata 
-                    app.counter = index;
-                    return app.userObj = element;
+                    //ricavo l'indice dall'array filtrata 
+                    return app.counter = index;
                 }
              });
         
         //aggiorna il valore per mostrarlo sull'header della chat 
-        app.userName = app.userObj.name;
-        app.userAvatar =  app.userObj.avatar;
-        app.userMsg = app.userObj.messages;
-        return app.userObj;
+        app.userName = app.contacts[app.counter].name;
+        app.userAvatar =  app.contacts[app.counter].avatar;
         },
     //MS3   
     //Quando richiamata fa comparire il messaggi inserito nella chat
         sendMessage: function(text){
            if(text !== ""){
-               app.inputMsg = {date: "10:00" , text: text, status: "sent"};
+               app.message = {date: dayjs().format('DD/MM/YYYY HH:mm:ss') , text: text, status: "sent"};
                //Il messaggio in <input> viene eliminato appena premuto [enter]
-               app.typingMsg = "";
-               //app.sentMessages.push(app.inputMsg);
-               app.contacts[app.counter].messages.push(app.inputMsg);
-               //app.contacts[username][]
+               app.typing = "";
+               app.contacts[app.counter].messages.push(app.message);
            }
-           //console.log(app.sentMessages);
-           console.log(app.contacts[app.counter].messages);
-           //setTimeout
-           return app.contacts;
+           //MS3 - Risposta automatica dopo un secondo
+           setTimeout(app.autoResponse, 1000);
         },
+        autoResponse: function(){
+            app.message = {date: dayjs().format('DD/MM/YYYY HH:mm:ss'), text: "ok", status: "received"};
+            app.contacts[app.counter].messages.push(app.message);
+        }
     },
 
 });
